52953f7734f68fa6cdeeb74f9eb2fd4078bf4f4b
Vinicius Jose Moreira Pacheco <vinicius_moreira06@hotmail.com>
2025-11-09 21:19:44 -0300

    criando microserviços algasensors

--- src/main/java/com/algaworks/algasensors/device/management/DeviceManagementApplication.java
+++ src/main/java/com/algaworks/algasensors/device/management/DeviceManagementApplication.java
@@ -2,8 +2,13 @@ package com.algaworks.algasensors.device.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class DeviceManagementApplication {
 
 	public static void main(String[] args) {
